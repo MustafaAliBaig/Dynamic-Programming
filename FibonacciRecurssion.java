1906c78cdba312428ecd64c3c32a4694e6e31f2b
MustafaAliBaig <mirzabaig.mustafaali@gmail.com>
2019-05-15 15:01:35 -0700

    Add files via upload

---
+++ FibonacciRecurssion.java
@@ -0,0 +1,15 @@
+/*formula F(n)=F(n-1)+F(n-2)*/
+public class FibonacciRecurssion {
+
+	static int Fib(int n)
+	{
+		if(n<=1)
+			return n;
+		return Fib(n-1)+Fib(n-2);
+	}
+	public static void main(String args[])
+	{
+		int n=35;
+		System.out.println(Fib(n));
+	}
+}
