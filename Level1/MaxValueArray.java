272fe7e3a9b3255f06840ac9a45222e567f9e4b1
mz564k <mz564k@CACDTL14JP925A.ITServices.sbc.com>
2020-05-26 19:24:17 -0700

    Program updated finding second highest-Solution done by Akheem Mehmood

--- Level1/MaxValueArray.java
+++ Level1/MaxValueArray.java
@@ -5,22 +5,36 @@ public class MaxValueArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*This is the program to write maximum value in an array */
+		/*This is the program to write maximum value in an array 
+		 * Program is updated with second highest value
+		 * here First Highest is Max=95
+		 * Second Highest is Max2=86
+		 * */
 		
-		int[] A= {10,2,4,6,7,95,56};
+		int[] A= {10,70,4,86,7,95,56};
 		
-		int max=Integer.MIN_VALUE;
+		//int max=Integer.MIN_VALUE;
+		int max=A[0];
+		int max2=A[0];
+
 		
 		for(int i=0;i<A.length;i++)
 		{
-			if(A[i]>max)
+			if(A[i]>max  )
 			{
-				max=A[i];
-			}
+				 //if(max2<max)
+			//{				
+				max2=max;
+			//}
+			max=A[i];
+		}
 		}
 		System.out.println("Maximum Value is "+max);
+
+		System.out.println(" Second Maximum Value is "+max2);
 		
 
 	}
+
 
 }
